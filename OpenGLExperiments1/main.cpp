7b8f6063cc542836ff2f865833034d84605ad38c
Ryan McCombe <ryanmccombe@gmail.com>
2019-12-31 17:43:55 +0000

    Window class

--- OpenGLExperiments1/main.cpp
+++ OpenGLExperiments1/main.cpp
@@ -1,79 +1,29 @@
 // Hides the console in Windows
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
-
 #include "shader.h"
 #include "vao.h"
+#include "window.h"
 
-void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 GLFWwindow* initialiseWindow();
-void initialiseGLAD();
 void render(GLFWwindow* window);
-void processInput(GLFWwindow* window);
-
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
-	const auto window = initialiseWindow();
-	initialiseGLAD();
+	const auto window = new Window();
 
 	glUseProgram(Shader::Program());
 	glBindVertexArray(VAO::Square());
 	
 	// The render loop
-	while (!glfwWindowShouldClose(window))
+	while (window->isRunning())
 	{
-		processInput(window);
-		render(window);
+		render(window->glfwWindow);
 	}
 
 	// Cleanup
 	glfwTerminate();
 	return 0;
-}
-
-// Create a GLFW window
-GLFWwindow* initialiseWindow()
-{
-	glfwInit();
-
-	// Which version of OpenGL are we using?
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-
-	// Which profile?  CORE has a smaller subset of open GL features, without
-	// legacy support
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	// Starting dimensions of the window
-	const auto window =
-		glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Experiments", nullptr, nullptr);
-
-	// Window can be resized by user, so we register a callback for when that
-	// happens
-	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
-
-	// Make this window the main context on the current thread
-	glfwMakeContextCurrent(window);
-
-	return window;
-}
-
-// GLAD manages function pointers for OpenGL - this is OS-specific
-void initialiseGLAD()
-{
-	// glfwGetProcAddress defines the correct function based on the OS we're
-	// compiling for
-	const auto loadProc = GLADloadproc(glfwGetProcAddress);
-	if (!gladLoadGLLoader(loadProc))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-	}
 }
 
 // The render loop
@@ -101,19 +51,4 @@ void render(GLFWwindow* window)
 	glfwPollEvents();
 }
 
-void processInput(GLFWwindow* window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-}
 
-// This gets called when the window is first created, and every time it is
-// subsequently resized
-void framebufferSizeCallback(GLFWwindow* window, const int width,
-                             const int height)
-{
-	// First 2 arguments - (0, 0) - defines where the bottom left corner of the
-	// rendering view is, within the parent window Second 2 arguments are the
-	// width and height, ie, the top right will be (0 + width, 0 + height)
-	glViewport(0, 0, width, height);
-}
---
+++ OpenGLExperiments1/window.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <iostream>
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+class Window
+{
+public:
+	GLFWwindow* glfwWindow;
+	
+	Window()
+	{
+		glfwInit();
+
+		// Which version of OpenGL are we using?
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+		// Which profile?  CORE has a smaller subset of open GL features, without
+		// legacy support
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+		// Starting dimensions of the window
+		const auto window =
+			glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Experiments", nullptr, nullptr);
+
+		// Window can be resized by user, so we register a callback for when that
+		// happens
+		glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+		// Make this window the main context on the current thread
+		glfwMakeContextCurrent(window);
+
+		glfwWindow = window;
+
+		initialiseGLAD();
+	}
+
+	bool isRunning()
+	{
+		return !glfwWindowShouldClose(glfwWindow);
+	}
+
+private:
+	void processInput(GLFWwindow* window)
+	{
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, true);
+	}
+
+	// This gets called when the window is first created, and every time it is
+	// subsequently resized
+	static void framebufferSizeCallback(GLFWwindow* window, const int width,
+	                                    const int height)
+	{
+		// First 2 arguments - (0, 0) - defines where the bottom left corner of the
+		// rendering view is, within the parent window Second 2 arguments are the
+		// width and height, ie, the top right will be (0 + width, 0 + height)
+		glViewport(0, 0, width, height);
+	}
+
+	// GLAD manages function pointers for OpenGL - this is OS-specific
+	static void initialiseGLAD()
+	{
+		// glfwGetProcAddress defines the correct function based on the OS we're
+		// compiling for
+		const auto loadProc = GLADloadproc(glfwGetProcAddress);
+		if (!gladLoadGLLoader(loadProc))
+		{
+			std::cout << "Failed to initialize GLAD" << std::endl;
+		}
+}
+};
