55cf2c69b66ad47c5cac8032e0aed6ae85483b69
Ryan McCombe <ryanmccombe@gmail.com>
2020-10-20 19:51:24 +0100

    Refactoring 1 - Cube Class

---
+++ OpenGLExperiments1/Renderer/Mesh/Cube.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <glad/glad.h>
+#include <vector>
+
+class Cube
+{
+public:
+	Cube()
+	{
+		glGenVertexArrays(1, &vertexArrayObject);
+		glGenBuffers(1, &vertexBufferObject);
+
+		// position attribute
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), static_cast<void*>(nullptr));
+		glEnableVertexAttribArray(0);
+		// texture coord attribute
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+
+		// std::cout << "Binding Cube" << vertexArrayObject << vertexBufferObject << std::endl;
+		glBindVertexArray(vertexArrayObject);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	}
+
+	void Bind()
+	{
+		
+	}
+	
+	float vertices[180] = {
+			-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+			0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+			0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+			0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+			-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+			0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+			0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+			0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+			-0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+
+			-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+			-0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+			-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+			0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+			0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+			0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+			0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+			0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+			-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+			0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+			0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+			-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+			-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+			0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+			0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+			0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+			-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+			-0.5f, 0.5f, -0.5f, 0.0f, 1.0f
+		};
+	unsigned int vertexArrayObject;
+	unsigned int vertexBufferObject;
+};
--- OpenGLExperiments1/main.cpp
+++ OpenGLExperiments1/main.cpp
@@ -1,5 +1,5 @@
 // Hides the console in Windows
-#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+// #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
 #include "shader.h"
 #include "vao.h"
@@ -12,46 +12,34 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-struct RenderObject
-{
-	Shader Shader;
-	unsigned int VAO;
-};
-
-template <std::size_t array_size>
-void render(Window* window, RenderObject (&objectsToRender)[array_size]);
+void render(Window* window, Shader* shader);
 
 int main()
 {
 	const auto window = new Window();
 
-	const Shader shader("../../vertex.shader", "../../fragment.shader");
-	shader.AddTexture("../../container.jpg");
-	shader.AddTexture("../../thinking.png", GL_RGBA, GL_TEXTURE1);
-
-	RenderObject objects[]
-	{
-		{
-			shader,
-			VAO::Square()
-		}
-	};
+	
 
 	glEnable(GL_DEPTH_TEST);
+
+	Shader shader("../../vertex.shader", "../../fragment.shader");
+	shader.AddTexture("../../container.jpg");
+	shader.AddTexture("../../thinking.png", GL_RGBA, GL_TEXTURE1);
 
 	// The render loop
 	while (window->isRunning())
 	{
-		render(window, objects);
+		render(window, &shader);
 	}
 
 	return 0;
 }
 
 // The render loop
-template <std::size_t array_size>
-void render(Window* window, RenderObject (&objectsToRender)[array_size])
+void render(Window* window, Shader* shader)
 {
+	
+	
 	// OpenGL is a state machine - this is a state setting function - we define
 	// the colour to use for all future clears
 	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
@@ -61,8 +49,7 @@ void render(Window* window, RenderObject (&objectsToRender)[array_size])
 	// - we're clearing the colour buffer
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	for (auto object : objectsToRender)
-	{
+	
 		// TODO: this is experimental matrix transformation code
 		glm::mat4 model = glm::mat4(1.0f);
 		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
@@ -77,16 +64,16 @@ void render(Window* window, RenderObject (&objectsToRender)[array_size])
 		glm::mat4 projection;
 		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 		
-		object.Shader.setMatrix("model", glm::value_ptr(model));
-		object.Shader.setMatrix("view", glm::value_ptr(view));
-		object.Shader.setMatrix("projection", glm::value_ptr(projection));
+		shader->setMatrix("model", glm::value_ptr(model));
+		shader->setMatrix("view", glm::value_ptr(view));
+		shader->setMatrix("projection", glm::value_ptr(projection));
 		// TODO: end of test code
 
-		object.Shader.Use();
+		shader->Use();
 		// glBindVertexArray(object.VAO);
 		// glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-		VAO::DrawTestCubes(&object.Shader, window);
-	}
+		VAO::DrawTestCubes(shader, window);
+	
 
 	// OpenGL uses double buffers - we display the "front" frame whilst creating
 	// the back frame Once the back frame is complete (ie, now), we swap them
